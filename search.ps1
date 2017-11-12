72fbfba121995b8c2802f852beae29bde3c6dd4c
Desition <31416930+Desition@users.noreply.github.com>
2017-11-12 13:24:19 +0100

    Add files via upload

---
+++ search.ps1
@@ -0,0 +1,37 @@
+$AvailableUpdates = @() 
+$UpdateIds = @() 
+$UpdateTypes 
+
+
+$Session = New-Object -com "Microsoft.Update.Session"
+
+Write-Host "Searching for updates..." 
+$Search = $Session.CreateUpdateSearcher()
+$SearchResults = $Search.Search("IsInstalled=0 and IsHidden=0")
+Write-Host "There are " $SearchResults.Updates.Count "TOTAL updates available."
+$AvailableUpdates = $SearchResults.Updates
+Write-Host "Updates selected for installation"
+
+$AvailableUpdates | ForEach-Object {
+	if (($_.InstallationBehavior.CanRequestUserInput) -or ($_.EulaAccepted -eq $FALSE)) { 
+            Write-Host $_.Title " *** Requires user input and will not be installed." -ForegroundColor Yellow 
+            if($ShowCategories) 
+            { 
+                $_.Categories | ForEach-Object {Write-Host "     "$_.Name.ToString() -ForegroundColor Cyan} 
+                 
+            } 
+                 
+        } 
+        else { 
+            Write-Host $_.Title -ForegroundColor Green 
+            if($ShowCategories) 
+            { 
+                $_.Categories | ForEach-Object {Write-Host "     "$_.Name.ToString() -ForegroundColor Cyan} 
+                 
+            } 
+        } 
+    }
+if($AvailableUpdates.count -lt 1){ 
+    Write-Host "No results meet your criteria. Exiting"; 
+	break 
+} 
