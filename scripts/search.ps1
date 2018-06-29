2d784b9a98d7c6d633fb49897b15263206d88e19
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2018-06-29 18:25:12 +0200

    Update search.ps1

--- scripts/search.ps1
+++ scripts/search.ps1
@@ -74,7 +74,7 @@ function install-update {
     Write-Host "`ndownloading Updates..."
     $NumUp=0
     foreach ($update in $result.Updates){
-        Write-Progress -Activity "Downloading Updates ..." -Status ($update.title) -PercentComplete ([int]($NumUp/$result.Updates.count*100)) -CurrentOperation [ $NumUp ]
+        Write-Progress -Activity "Downloading Updates ..." -Status ($update.title) -PercentComplete ([int]($NumUp/$result.Updates.count*100)) -CurrentOperation "[ $NumUp ]"
         
 	    $downloads = New-Object -ComObject Microsoft.Update.UpdateColl
         $downloads.Add($update)|out-null
@@ -87,7 +87,7 @@ function install-update {
     Write-Host "`ninstalling Updates..."
     $NumUp=0
     foreach ($update in $result.Updates){
-        Write-Progress -Activity "Installing Updates ..." -Status ($update.title) -PercentComplete([int]($NumUp/$result.Updates.count*100)) -CurrentOperation [ $NumUp ]
+        Write-Progress -Activity "Installing Updates ..." -Status ($update.title) -PercentComplete([int]($NumUp/$result.Updates.count*100)) -CurrentOperation "[ $NumUp ]"
 	
 	    $installs = New-Object -ComObject Microsoft.Update.UpdateColl
         if ($update.IsDownloaded){
