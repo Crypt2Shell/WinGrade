7b2f27cb1542027cce0563a163ba07d8f4b317cd
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2019-06-20 18:51:01 +0200

    Update search.ps1

--- scripts/search.ps1
+++ scripts/search.ps1
@@ -168,10 +168,10 @@ function get-reboot {
         write-host "`n["-nonewline; write-host "*" -ForegroundColor Cyan -nonewline; write-host "] "-nonewline; Write-Host "No reboot required."
         #del "$env:tmp\WinGrade.bat" -ErrorAction SilentlyContinue
 	del "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WinGrade.bat" -ErrorAction SilentlyContinue
-	net user admintestuser /delete -ErrorAction SilentlyContinue
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "AutoAdminLogon" /t "REG_SZ" /d "0" /f -ErrorAction SilentlyContinue
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultUserName" /t "REG_SZ" /d "" /f -ErrorAction SilentlyContinue
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultPassword" /t "REG_SZ" /d "" /f -ErrorAction SilentlyContinue
+	net user admintestuser /delete
+	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "AutoAdminLogon" /t "REG_SZ" /d "0" /f
+	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultUserName" /t "REG_SZ" /d "" /f
+	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultPassword" /t "REG_SZ" /d "" /f
         #schtasks /delete /F /TN "WinGrade"
         elevate-privileges
     }
