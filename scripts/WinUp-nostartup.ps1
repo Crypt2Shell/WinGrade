03935a66ce122b30a1bbb983ab1a1e0f04ce4e3d
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2019-06-24 09:28:40 +0200

    Update WinUp-nostartup.ps1

--- scripts/WinUp-nostartup.ps1
+++ scripts/WinUp-nostartup.ps1
@@ -163,8 +163,8 @@ function get-reboot {
         write-host "`n["-nonewline; write-host "*" -ForegroundColor Cyan -nonewline; write-host "] "-nonewline; Write-Host "No reboot required."
 	del "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WinGrade.bat" -ErrorAction SilentlyContinue
 	net user Administrator /active:no
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "AutoAdminLogon" /t "REG_SZ" /d "0" /f
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultUserName" /t "REG_SZ" /d " " /f
+	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /f /v "AutoAdminLogon" /t "REG_SZ" /d "0"
+	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /f /v "DefaultUserName" /t "REG_SZ" /d " "
 	reg DELETE "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultPassword" /f
         elevate-privileges
     }
