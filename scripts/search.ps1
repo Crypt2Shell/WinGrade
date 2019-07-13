78a00a1e719034bb79340fdeec3cdcbaa7e8e3d2
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2019-07-13 17:06:29 +0200

    Update search.ps1

--- scripts/search.ps1
+++ scripts/search.ps1
@@ -162,11 +162,6 @@ function get-reboot {
     }
     else { 
         write-host "`n["-nonewline; write-host "*" -ForegroundColor Cyan -nonewline; write-host "] "-nonewline; Write-Host "No reboot required."
-	#del "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WinGrade.bat" -ErrorAction SilentlyContinue
-	net user Administrator /active:no
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "AutoAdminLogon" /t "REG_SZ" /d "0" /f
-	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultUserName" /t "REG_SZ" /f
-	reg DELETE "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultPassword" /f
 	del "$env:windir\WinGrade.bat" -ErrorAction SilentlyContinue
 	schtasks /delete /F /TN "WinGrade"
         elevate-privileges
