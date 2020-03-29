3ae6ee1290343980c9a06f3e2aa936bf60c46594
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2020-03-29 21:27:16 +0200

    Update search.ps1

--- scripts/search.ps1
+++ scripts/search.ps1
@@ -159,7 +159,11 @@ function get-reboot {
     if($key -ne $null -or $installresult.rebootRequired) {
         write-host "`n["-nonewline; write-host "*" -ForegroundColor Cyan -nonewline; write-host "] "-nonewline; Write-Host "Rebooting..."
         net user Administrator WinGrade /active:yes
-	Start-BitsTransfer https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade.bat "$env:windir\WinGrade.bat"
+	$url = "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade.bat"
+	$output = "$env:windir\WinGrade.bat"
+	$start_time = Get-Date
+	(New-Object System.Net.WebClient).DownloadFile($url, $output)
+	Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "AutoAdminLogon" /t "REG_SZ" /d "1" /f
 	#reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultDomainName"/t "REG_SZ" /f
 	reg ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultUserName" /t "REG_SZ" /d "Administrator" /f
