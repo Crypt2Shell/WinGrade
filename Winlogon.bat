0dcae3c833ffd0b63ee92e3927a57e8ca782981a
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2020-08-17 20:30:13 +0200

    Update Winlogon.bat

--- Winlogon.bat
+++ Winlogon.bat
@@ -22,7 +22,6 @@ if %errorlevel% NEQ 0 (
 		bitsadmin /util /setieproxy localsystem AUTODETECT
 		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-winlogon.bat" "%tmp%\WinGrade.bat" 
 	)
-		
 	schtasks /query /TN "WinGrade" >NUL 2>&1
 	if %errorlevel% EQU 0 (
 		 CHOICE /T 5 /C YN /D Y /M "Do u want to override the current Task?"
@@ -33,41 +32,42 @@ if %errorlevel% NEQ 0 (
 		 ) else if errorlevel == 1 (
 			 :: YES
 			 schtasks /create /tn "WinGrade" /SC onstart /RU "SYSTEM" /DELAY 0000:30 /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /x /d /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
-			 echo [+] WinGrade Task installed!
-		 	 timeout /t 5 /nobreak>NUL
-			 shutdown /r /t 0
+			 goto check-WinGrade-Installation
 		 )
 	)
 	schtasks /query /TN "WinGrade" >NUL 2>&1
 	if %errorlevel% NEQ 0 ( 
 		schtasks /create /tn "WebClient" /SC onstart /RU "SYSTEM" /DELAY 0000:05 /RL highest /F /TR "net start webclient"
 		schtasks /create /tn "WinGrade" /SC onstart /RU "SYSTEM" /DELAY 0000:30 /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /x /d /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
-
-		schtasks /query /TN "WinGrade" >NUL 2>&1
-		if %errorlevel% EQU 0 (
-			echo [+] WinGrade Task installed!
-			timeout /t 5 /nobreak>NUL
-			shutdown /r /t 0
-		) else (
-			echo [*] Something went wrong pls try again...
-		 	echo [-] Task not installed!!!
-			timeout /t 5 /nobreak>NUL
-		)
+		goto check-WinGrade-Installation
 	) else ( 
 		echo [*] checking for currently installed WinGrade Task...
 		timeout /t 5 /nobreak>NUL
-		schtasks /query /TN "WinGrade" >NUL 2>&1
-		if %errorlevel% EQU 0 (
-			echo [+] FOUND: WinGrade Task installed!
-			timeout /t 5 /nobreak>NUL
-			echo [*] TRYING: to run Wingrade Task...
-			timeout /t 3 /nobreak>NUL
-			echo [+] WinGrade Task installed!
-			timeout /t 5 /nobreak>NUL
-			shutdown /r /t 0
-		) else (
-			echo [*] Something went wrong pls try again...
-		 	echo [-] Task not installed!!!
-		 	timeout /t 5 /nobreak>NUL
-		)
+		goto check-WinGrade-Installation
+	)
+
+:check-WinGrade-Installation
+	bitsadmin /util /setieproxy localsystem AUTODETECT
+	bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-winlogon.bat" "%tmp%\WinGrade.bat"
+	if exist "%tmp%\Wingrade.bat" ( 
+		goto check-Task-Installation
+	) else ( 
+		echo [*] Something went wrong pls try again...
+		echo [-] WinGrade could not be downloaded!
+		timeout /t 5 /nobreak>NUL 
+	)
+
+:check-Task-Installation
+	schtasks /query /TN "WinGrade" >NUL 2>&1
+	if %errorlevel% EQU 0 (
+		echo [+] FOUND: WinGrade Task installation!
+		timeout /t 5 /nobreak>NUL
+		echo [*] TRYING: to run Wingrade Task...
+		timeout /t 3 /nobreak>NUL
+		shutdown /r /t 0
+		if %errorlevel% NEQ 0 ( pause )
+	) else (
+		echo [*] Something went wrong pls try again...
+		echo [-] Task not installed!!!
+		timeout /t 5 /nobreak>NUL
 	)
