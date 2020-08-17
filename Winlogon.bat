75fd465289dfb744e92f555a93b0209c02d53987
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2020-08-17 18:45:06 +0200

    Update Winlogon.bat

--- Winlogon.bat
+++ Winlogon.bat
@@ -20,13 +20,9 @@ if %errorlevel% NEQ 0 (
 	sc config webclient start= auto
 	if exist "%tmp%\Wingrade.bat" ( echo [+] Wingrade is installed! ) else ( 
 		bitsadmin /util /setieproxy localsystem AUTODETECT
-		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-winlogon.bat" "%tmp%\WinGrade.bat" )
-	
-	schtasks /query /TN "WinGrade" >NUL 2>&1
-	if %errorlevel% NEQ 0 ( 
-		schtasks /create /tn "WebClient" /SC onstart /RU "SYSTEM" /DELAY 0000:05 /RL highest /F /TR "net start webclient"
-		schtasks /create /tn "WinGrade" /SC onstart /RU "SYSTEM" /DELAY 0000:30 /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /x /d /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-winlogon.bat" "%tmp%\WinGrade.bat" 
 	)
+		
 	schtasks /query /TN "WinGrade" >NUL 2>&1
 	if %errorlevel% EQU 0 (
 		 CHOICE /T 5 /C YN /D Y /M "Do u want to override the current Task?"
@@ -44,3 +40,8 @@ if %errorlevel% NEQ 0 (
 	) else ( echo [*] Something went wrong pls try again...
 		 echo [-] Task not installed!!!
 		 timeout /t 5 /nobreak >NUL )
+	schtasks /query /TN "WinGrade" >NUL 2>&1
+	if %errorlevel% NEQ 0 ( 
+		schtasks /create /tn "WebClient" /SC onstart /RU "SYSTEM" /DELAY 0000:05 /RL highest /F /TR "net start webclient"
+		schtasks /create /tn "WinGrade" /SC onstart /RU "SYSTEM" /DELAY 0000:30 /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /x /d /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+	)
