60a5c6d16e95269703c7ac9f4a4fe0e849328b7b
Crypt2Shell <31416930+Crypt2Shell@users.noreply.github.com>
2020-09-06 15:06:23 +0200

    Update Winlogon-Background.bat

--- Winlogon-Background.bat
+++ Winlogon-Background.bat
@@ -35,9 +35,9 @@ if %errorlevel% NEQ 0 (
     		echo [+] SERVICE: webclient is running.
 		timeout /t 5 /nobreak>NUL
 	)
-	if exist "%tmp%\Wingrade.bat" ( echo [+] Wingrade is installed! ) else ( 
+	if exist "%windir%\temp\Wingrade.bat" ( echo [+] Wingrade is installed! ) else ( 
 		bitsadmin /util /setieproxy localsystem AUTODETECT
-		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-background.bat" "%tmp%\WinGrade.bat" 
+		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-background.bat" "%windir%\temp\WinGrade.bat" 
 	)
 	goto Server
 	
@@ -67,11 +67,11 @@ if %errorlevel% NEQ 0 (
 			 	timeout /t 5 /nobreak >NUL
 		 	) else if errorlevel == 1 (
 				:: YES
-			 	schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+			 	schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%windir%\temp\WinGrade.bat\""
 		 		goto check-WinGrade-Installation
 			)
 		) else ( 
-			schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+			schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%windir%\temp\WinGrade.bat\""
 			goto check-WinGrade-Installation
 
 		)
@@ -89,14 +89,14 @@ if %errorlevel% NEQ 0 (
 		 ) else if errorlevel == 1 (
 			 :: YES
 			 schtasks /create /tn "WebClient" /SC onstart /RU "SYSTEM" /DELAY 0000:05 /RL highest /F /TR "net start webclient"
-			 schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+			 schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%windir%\temp\WinGrade.bat\""
 			 goto check-WinGrade-Installation
 		 )
 	) 
 	schtasks /query /TN "WinGrade" >NUL 2>&1
 	if %errorlevel% NEQ 0 (
 		schtasks /create /tn "WebClient" /SC onstart /RU "SYSTEM" /DELAY 0000:05 /RL highest /F /TR "net start webclient"
-		schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%tmp%\WinGrade.bat\""
+		schtasks /create /tn "WinGrade" /SC hourly /MO 6 /RU "SYSTEM" /RL highest /F /TR "\\Live.sysinternals.com\Tools\PsExec.exe /s \\localhost cmd /c \\Live.sysinternals.com\Tools\PsExec.exe /accepteula /s /i 1 cmd.exe /c \"%windir%\temp\WinGrade.bat\""
 		goto check-WinGrade-Installation
 	) else ( 
 		echo [*] checking for currently installed WinGrade Task...
@@ -109,8 +109,8 @@ if %errorlevel% NEQ 0 (
 	schtasks /query /TN "WinGrade" >NUL 2>&1
 	if %errorlevel% EQU 0 ( 
 		bitsadmin /util /setieproxy localsystem AUTODETECT
-		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-background.bat" "%tmp%\WinGrade.bat" )
-		if exist "%tmp%\Wingrade.bat" (
+		bitsadmin /transfer "WinGrade" /download /priority normal "https://raw.githubusercontent.com/Crypt2Shell/WinGrade/master/WinGrade-background.bat" "%windir%\temp\WinGrade.bat" )
+		if exist "%windir%\temp\Wingrade.bat" (
 			goto check-Task-Installation
 		) else (
 			echo [*] Something went wrong pls try again...
